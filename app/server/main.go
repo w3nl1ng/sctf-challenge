e512e688c8c96065f675cf6b1660a45cedfb528a
w3nl1ng <quanjunxiong@gmail.com>
2023-05-06 17:28:47 +0800

    finish most parts of server

--- app/server/main.go
+++ app/server/main.go
@@ -1,5 +1,120 @@
 package main
 
+import (
+	"net/http"
+
+	"database/sql"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// type userInfo struct {
+// 	ID    int    `json:"id"`
+// 	Name  string `json:"name"`
+// 	Phone string `json:"phone"`
+// }
+
 func main() {
-	
+	router := gin.Default()
+
+	authRouter := router.Group("/auth")
+	authRouter.Use(AuthMiddleware())
+	authRouter.GET("/showaddressbook", showAddressBook)
+
+	nologinRouter := router.Group("/nologin")
+	nologinRouter.GET("/userinfo", queryUserInfoByName)
+
+	router.Run(":8080")
+}
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		userId := ctx.Query("userid")
+		password := ctx.Query("password")
+
+		if password == "" || userId == "" {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error":"please specify userid and username"})
+			return
+		}
+		if ok := checkLogin(userId, password); !ok {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "username or password is wrong"})
+			return
+		}
+		ctx.Next()
+	}
+}
+
+func checkLogin(userId, password string) bool {
+	//建立数据库连接
+	db, err := sql.Open("mysql", "root:yiqiu666...@tcp(127.0.0.1:3306)/sctf_db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	stmtout, err := db.Prepare("SELECT password FROM users WHERE id = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmtout.Close()
+
+	var storedPwd string
+	err = stmtout.QueryRow(userId).Scan(&storedPwd)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if password == storedPwd {
+		return true
+	} else {
+		return false
+	}
+}
+
+func queryUserInfoByName(ctx *gin.Context) {
+	userName := ctx.Query("username")
+	if userName == "" {
+		log.Print("username not set")
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "username not set"})
+		return
+	}
+
+	//打开数据库连接
+	db, err := sql.Open("mysql", "root:yiqiu666...@tcp(127.0.0.1:3306)/sctf_db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("select id, name, phone from info where name='" + userName + "'")
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var userId int
+		var userName string
+		var userPhone string
+		err = rows.Scan(&userId, &userName, &userPhone)
+		if err != nil {
+			log.Print(err)
+			return
+		}
+
+		log.Print(userId, userName, userPhone)
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"id":userId,
+			"name":userName,
+			"phone":userPhone,
+		})
+	}
+}
+
+func showAddressBook(ctx *gin.Context) {
+	ctx.String(http.StatusOK, "this is your adress book")
 }
